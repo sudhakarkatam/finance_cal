fba3e91954f458b3761953f562e6f591506c22c2
sudhakarkatam <sudhakarkatam777@gmail.com>
2025-10-14 00:07:39 +0530

    icon and release issues fixed

---
+++ android/app/src/main/java/com/easecraft/financialcalculator/MainActivity.java
@@ -0,0 +1,5 @@
+package com.easecraft.financialcalculator;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.appcal',
+  appId: 'com.easecraft.financialcalculator',
   appName: 'Financial Calculator',
   webDir: 'dist',
   server: {
